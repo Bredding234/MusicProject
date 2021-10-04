927bfe27c53fcfd52ea4166574e5fc83dc7a488e
Bredding234 <39637656+Bredding234@users.noreply.github.com>
2021-10-04 13:15:28 -0400

    Update website.js

--- website.js
+++ website.js
@@ -3,42 +3,59 @@ imageOne.src = "./Images/br.jfif";
 //var timer = 0;
 //var myInterval = -1;
 
-function change()
-{
 
-    document.images[0].src = "./Images/drake.jpg";
-   	 imageOne = setTimeout(change2, 2000);
+// change method changes the picture every two seconds
+// function change()
+// {
+
+//     document.images[0].src = "./Images/drake.jpg";
+//    	 imageOne = setTimeout(change2, 2000);
 	 
-    }
-setTimeout(change, 2000);
+//     }
+// setTimeout(change, 2000);
 
-function change2(){
-   	 document.images[0].src = "./Images/em.jfif";
-   	 setTimeout(change3, 2000);
-}
+// // change method changes the 2nd picture to third every two seconds
 
-function change3(){
-   	 document.images[0].src = "./Images/nasx.jfif";
-}
+// function change2(){
+//    	 document.images[0].src = "./Images/em.jfif";
+//    	 setTimeout(change3, 2000);
+// }
+// // change method changes the 3rd picture to fourth every two seconds
 
-function setTimer(){
-// this pauses the slideshow.
-    clearTimeout(imageOne);
+// function change3(){
+//    	 document.images[0].src = "./Images/nasx.jfif";
+// }
+
+
+// function setTimer(){
+// // this pauses the slideshow.
+//     clearTimeout(imageOne);
    	 
-}
-function startSlide()
-{
-// this resumes the slide show.
-	imageOne = setTimeout(change, 500);
-}
+// }
+// function startSlide()
+// {
+// // this resumes the slide show.
+// 	imageOne = setTimeout(change, 500);
+// }
+// // the function 
+// function backToFirstImage(){
+// 	   	 document.images[0].src = "./Images/king.jfif";
+// }
 
-function backToFirstImage(){
-	   	 document.images[0].src = "./Images/king.jfif";
+
+// function restartSlides(){
+//    	 document.images[0].src = "./Images/nasx.jfif";
+// 	imageOne = setTimeout(backToFirstImage, 2000);
+// }
+
+// (A) WAIT FOR PAGE TO FULLY LOAD
+ 
+
+function searchButton(){
+  var searchBox = document.getElementById("fnames").value;
+  var pics1 = document.getElementById("pics1"); 
+  if(searchBox == "Drake"){
+      document.body.innerHTML += pics1; // prints out data
+      document.write(pics1)
+  }
 }
-
-
-function restartSlides(){
-   	 document.images[0].src = "./Images/nasx.jfif";
-	imageOne = setTimeout(backToFirstImage, 2000);
-}
-
